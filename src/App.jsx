4c0f5da8ee1c09f46aa62177abb938522673cab6
jozefhdez <jozefhdez2@gmail.com>
2025-04-04 13:16:19 -0600

    Updates styling and adds Prettier configuration file
    Modifies the App component to include a new CSS class for the container name, applying the 'Gill Sans' font and bold weight. Also, adds a new .prettierrc file for Prettier settings.
    
    Fixes #N/A

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-import './App.css'
-import Pad from './game/buttons/Pad'
-import Screen from './game/Screen'
-import StartSelect from './game/buttons/StartSelect'
-import Actions from './game/buttons/Actions'
+import "./App.css";
+import Pad from "./game/buttons/Pad";
+import Screen from "./game/Screen";
+import StartSelect from "./game/buttons/StartSelect";
+import Actions from "./game/buttons/Actions";
 
 function App() {
-
   return (
     <>
-      <div class = "container-main">
+      <div class="container-main">
         {/* Shadow */}
-        <div class = "shadow">
+        <div class="shadow">
           {/* Container top */}
-          <div class = "container-top"></div>
+          <div class="container-top"></div>
           {/* Container game */}
-          <div class = "container-gameboy">
+          <div class="container-gameboy">
             {/* Container screen */}
-            <div class = "container-screen-center">
-              <div class = "container-screen">
-                {/* Container top screen */}
-                <div class = "container-screen-title">
-                  <div class = "container-dash-title1"></div>
-                  <div class = "container-dash-title2"></div>
-                </div>
-                <Screen />
-              </div>
+            <div class="container-screen-center">
+              <Screen />
               {/* Container Nintendo */}
-              <div class = "container-name">
-              Nintendo GAMEBOY
-              </div>
+              <div class="container-name">Nintendo GAMEBOY</div>
             </div>
             {/* Container buttons */}
-            <div class = "container-buttons-center">
-              <div class = "container-buttons">
+            <div class="container-buttons-center">
+              <div class="container-buttons">
                 {/* Directions pad */}
                 <Pad />
                 {/*Select start*/}
@@ -45,7 +35,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/game/Screen.jsx
+++ src/game/Screen.jsx
@@ -1,6 +1,13 @@
 const Screen = () => {
     return (
+        <div class="container-screen">
+        {/* Container top screen */}
+        <div class="container-screen-title">
+          <div class="container-dash-title1"></div>
+          <div class="container-dash-title2"></div>
+        </div>
         <div class="container-display"></div>
+      </div>
     );
 };
 
